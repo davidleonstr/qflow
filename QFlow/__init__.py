b6fda1d141149f1ea69deb2b720d6c532a618341
davidleonstr <davidalfonsoleoncarmona@gmail.com>
2026-03-27 10:00:08 -0600

    Expose extensions module directly from QFlow

--- QFlow/__init__.py
+++ QFlow/__init__.py
@@ -4,9 +4,10 @@ from . import stores
 from . import helpers
 from . import hooks
 from . import core
+from . import extensions
 
 __all__ = [
-    'core', 'helpers', 'hooks', 'stores', 
+    'core', 'helpers', 'hooks', 'stores', 'extensions', 
     'components', 'App', 'app', 'Window', 'window',
     'Screen', 'screen', 'Template'
 ]
